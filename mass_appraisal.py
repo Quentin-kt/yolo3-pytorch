151b6edc67722cb6e70e0dc5a2fed2dedd4c15af
Quentin-kt <850027327@qq.com>
2021-05-14 20:34:21 +0800

    批量评估——第一次提交

---
+++ mass_appraisal.py
@@ -0,0 +1,29 @@
+import os
+
+
+def alter(file, old_str, new_str):
+    with open(file, "r", encoding="utf-8") as f1, open("%s.bak" % file, "w", encoding="utf-8") as f2:
+        for line in f1:
+
+            if old_str in line:
+                line = line.replace(old_str, new_str)
+
+            f2.write(line)
+    os.remove(file)
+    os.rename("%s.bak" % file, file)
+
+
+yolo = r"./yolo.py"
+get_map = r"./get_map.py"
+for idx in range(1, 11):
+    old_pth = "Epoch" + str(idx) + ".pth"
+    new_pth = "Epoch" + str(idx + 1) + ".pth"
+    old_result = 'results_files_path = "results' + str(idx)
+    new_result = 'results_files_path = "results' + str(idx + 1)
+    os.system("python ./get_dr_txt.py")
+    os.system("python ./get_gt_txt.py")
+    os.system("python ./get_map.py")
+    alter(yolo, old_pth, new_pth)
+    alter(get_map, old_result, new_result)
+alter(yolo, "Epoch11.pth", "Epoch1.pth")
+alter(get_map, 'results_files_path = "results11"', 'results_files_path = "results1"')
