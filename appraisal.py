09d5672b1e5474ef13eccb53f7370aef00dcab82
Quentin-kt <850027327@qq.com>
2021-05-18 15:28:19 +0800

    Update appraisal.py，评估结果存入

--- appraisal.py
+++ appraisal.py
@@ -1,16 +1,14 @@
 import os
 import torch
 import numpy as np
+import shutil
 
-"""
- 损失函数——多标签二分类交叉熵（Binary Cross Entropy）
-"""
 # 初始化
 classes = ["ChJ", "BX", "ZhAW", "CK", "QF", "TJ", "JG", "ShG", "PL", "ZhGAJ", "CQBG", "YWChR", "ShL", "FSh"]
 
 
 # 将文件夹内所有txt文件转换为一个tensor
-def file_lines_to_tensor(dir_path, confidence_whether):
+def file_lines_to_tensor(dir_path, whether_confidence):
     total_file_count = sum([len(files) for root, dirs, files in os.walk(dir_path)])
     numpy = np.zeros((total_file_count, len(classes)))
     for filepath, dir_path, filenames in os.walk(dir_path):
@@ -23,7 +21,7 @@ def file_lines_to_tensor(dir_path, confidence_whether):
                 for line in txt:
                     for idx_class in range(len(classes)):
                         if classes[idx_class] in line:
-                            if confidence_whether:
+                            if whether_confidence:
                                 confidence = float(line.split()[1])
                                 if confidence > max_confidence[idx_class]:
                                     max_confidence[idx_class] = confidence
@@ -35,7 +33,23 @@ def file_lines_to_tensor(dir_path, confidence_whether):
     return tensor
 
 
+"""
+ 评估
+    损失函数——多标签二分类交叉熵（Binary Cross Entropy）
+"""
 detection = file_lines_to_tensor(r'input/detection-results', True)
 truth = file_lines_to_tensor(r'input/ground-truth', False)
 loss = torch.nn.BCELoss()
-print(loss(detection, truth))
+total_loss = float(loss(detection, truth))
+print(total_loss)
+
+"""
+ 将评估结果存入results
+"""
+results_files_path = r'results/results_1'
+if os.path.exists(results_files_path):  # if it exist already
+    # reset the results directory
+    shutil.rmtree(results_files_path)
+os.makedirs(results_files_path)
+with open(results_files_path + "/results.txt", 'a') as results_file:
+    results_file.write("total = " + str(total_loss) + "\n")
