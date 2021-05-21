61846a4683546bb988549e45ec81deacf0203d66
Quentin-kt <850027327@qq.com>
2021-05-21 09:46:23 +0800

    Update get_dr_txt.py，评估结果可视化

--- get_dr_txt.py
+++ get_dr_txt.py
@@ -134,7 +134,7 @@ for image_id in tqdm(image_ids):
     image_path = "./VOCdevkit/VOC2007/JPEGImages/" + image_id + ".jpg"
     image = Image.open(image_path)
     # 开启后在之后计算mAP可以可视化
-    # image.save("./input/images-optional/"+image_id+".jpg")
+    # image.save("./input/images-optional/"+image_id+".png")
     yolo.detect_image(image_id, image)
 
 print("Conversion completed!")
